2626839b6cfa83a16c512c0e66f457b9ea59fed6
Aleksey Litvinov <lyosh.2010@mail.ru>
2024-01-21 11:55:20 +0300

    tets

--- Main.java
+++ Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String[] args) {
         TeacherService name = new TeacherService();
         TeacherView view = new TeacherView();
+
         TeacherController controller = new TeacherController(name, view);
 
         controller.run();
